e20a16fa305e9a1d3708bf40917d1575287474c6
AnJicheol <90523400+AnJicheol@users.noreply.github.com>
2022-04-12 00:22:08 +0900

    Create sorted-sum_T.java

---
+++ 일일/_12.두 개 뽑아서 더하기/sorted-sum_T.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        
+        HashSet<Integer> set = new HashSet<>();
+ 
+        
+        for(int i = 0; i < numbers.length - 1; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                  set.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        Integer[] arr = set.toArray(new Integer[0]);
+        int answer[] = new int[set.size()];
+        
+        for(int i = 0; i < set.size(); i++){
+            answer[i] = arr[i];
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
+
+최고 속도 1.17ms
