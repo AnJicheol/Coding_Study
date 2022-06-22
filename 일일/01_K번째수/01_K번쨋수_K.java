9690220f711f9d4bf6b71e3ad36d44115e4722fa
AnJicheol <90523400+AnJicheol@users.noreply.github.com>
2022-06-22 19:24:34 +0900

    Update 01_K번쨋수_K.java
    안지철 피드백

--- 일일/01_K번째수/01_K번쨋수_K.java
+++ 일일/01_K번째수/01_K번쨋수_K.java
@@ -18,7 +18,8 @@ class Solution {
                 l ++;
             }
             int up = 0;
-            for (int num1 = 0; num1 < result.length; num1 ++) {
+            
+            for (int num1 = 0; num1 < result.length; num1 ++) {            
                 up = num1;
                 for (int num2 = 0; num2 < result.length - up; num2 ++) {
                     if (result[num2] > result[num2 + 1]) {
@@ -36,3 +37,12 @@ class Solution {
         return answer;
     }
 }
+
+
+//
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 피드백 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+안지철
+[22번줄에 정렬 알고리즘 버블 정렬 알고리즘임
+해당 알고리즘은 이미 정렬된 곳도 if문으로 비교하기 때문에 
+비효율적인 다른 방법으로 개선 필요해보임]
+
