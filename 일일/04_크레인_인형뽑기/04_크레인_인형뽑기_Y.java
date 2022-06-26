a1f9e0a79916b526279fac372cde07847ac1e866
kyhhhhhh <107259622+kyhhhhhh@users.noreply.github.com>
2022-06-26 14:15:54 +0900

    Create 04_크레인_인형뽑기_Y.java

---
+++ 일일/04_크레인_인형뽑기/04_크레인_인형뽑기_Y.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        int[] box = new int[moves.length]; // moves의 길이만큼 배열 크기를 정함
+        int index = 0; //box의 index
+        
+        
+        for(int i = 0; i < moves.length;i++){
+            for(int j = 0; j < board.length; j++){
+                
+                if(board[j][moves[i]-1] != 0){ //1,5,3,5,1,2,1,4; 1부터 시작하기 때문에 -1을해줌
+                    
+                    box[index] = board[j][moves[i]-1];
+                    board[j][moves[i]-1] = 0;
+                    
+                    if(index != 0 && box[index-1] == box[index]){ // 2값이 연속으로 동일한 숫자일 경우
+                        box[index-1] = 0;
+                        box[index] = 0;
+                        index = index -2;
+                        answer = answer+2;
+                    }
+                    
+                    index++;
+                    break;
+                }
+            }
+        }
+        /* 테스트코드
+        System.out.print(" box의 값 확인 : " + " ");
+        for(int i=0; i < box.length; i++){
+            System.out.print(box[i]);
+        }
+        */
+        
+        return answer;
+    }
+}
