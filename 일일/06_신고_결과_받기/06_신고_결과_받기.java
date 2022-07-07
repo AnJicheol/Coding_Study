b73b880336da05e4ba6db52b211adae78a1d6166
AnJicheol <90523400+AnJicheol@users.noreply.github.com>
2022-07-07 21:28:41 +0900

    Create 06_신고_결과_받기.java

---
+++ 일일/06_신고_결과_받기/06_신고_결과_받기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+
+        int[] answer = new int[id_list.length];  
+        Map<String, Integer> index = new HashMap<>();
+        Map<String, HashSet<String>> map = new HashMap<>();
+
+        for (int i = 0; i < id_list.length; i++) {
+              String id = id_list[i];
+              map.put(id, new HashSet<>());
+              index.put(id, i);
+        }
+
+        for(String ID : report){                 
+              String[]  CH = ID.split(" ");
+              map.get(CH[1]).add(CH[0]);       
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            HashSet<String> len = map.get(id_list[i]);
+            if (len.size() >= k) {
+                for (String id : len) {
+                    answer[index.get(id)]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
