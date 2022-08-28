1405de999d87f01d6c30cc15fdc947a309a8cc96
kyhhhhhh <107259622+kyhhhhhh@users.noreply.github.com>
2022-08-28 20:19:14 +0900

    Create  12_문자열_압축_Y.java

---
+++ 일일/12_문자열_압축/12_문자열_압축_Y.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int solution(String s) {
+        int answer = s.length();
+        int count = 0;
+        int num = 0;
+        int index = 0;
+
+        if(s.length() <= 2){ return s.length(); } // 문자열이 2글자 이하라면 길이 리턴
+
+        for(int len = 1; len <= s.length() /2; len++){
+            num = s.length()%len; // 남는 문자열 길이
+            String string = s.substring(0,len);
+
+            for(int j = 0; j < s.length()/len; j+=count){
+                count = 0;
+                for(int start = index; start <= s.length()-len; start +=len){
+                    //System.out.println(string);
+                    if(string.equals(s.substring(start, start+len))){ //문자열이 연속될 경우
+                        count++;
+                        index += len;
+                    }else{ // 연속되지 않을 경우
+                        string = s.substring(start,start+len);
+                        break;
+                    }
+                }
+                if(count >= 2){ num += (int)(Math.log10(count)+1) + string.length();}
+                else{num += string.length();}
+
+                if(num > answer){break;} //실행도중 answer보다 큰 값이 되었다면 break
+            }
+            if(num < answer){ answer = num; }
+            count = 0;
+            index = 0;
+        }
+        return answer;
+    }
+}
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+테스트 1 〉	통과 (0.07ms, 75.1MB)
+테스트 2 〉	통과 (0.19ms, 76MB)
+테스트 3 〉	통과 (0.10ms, 72.3MB)
+테스트 4 〉	통과 (0.05ms, 74.6MB)
+테스트 5 〉	통과 (0.02ms, 74.1MB)
+테스트 6 〉	통과 (0.06ms, 72.9MB)
+테스트 7 〉	통과 (0.33ms, 75.6MB)
+테스트 8 〉	통과 (0.32ms, 74.9MB)
+테스트 9 〉	통과 (0.49ms, 75.8MB)
+테스트 10 〉	통과 (1.97ms, 86.6MB)
+테스트 11 〉	통과 (0.10ms, 78MB)
+테스트 12 〉	통과 (0.12ms, 74.2MB)
+테스트 13 〉	통과 (0.14ms, 75MB)
+테스트 14 〉	통과 (0.45ms, 73.9MB)
+테스트 15 〉	통과 (0.15ms, 69.9MB)
+테스트 16 〉	통과 (0.03ms, 75.7MB)
+테스트 17 〉	통과 (0.96ms, 72.7MB)
+테스트 18 〉	통과 (1.01ms, 74.9MB)
+테스트 19 〉	통과 (1.07ms, 76.8MB)
+테스트 20 〉	통과 (2.19ms, 75.9MB)
+테스트 21 〉	통과 (2.76ms, 73.6MB)
+테스트 22 〉	통과 (2.61ms, 73.7MB)
+테스트 23 〉	통과 (1.99ms, 73.4MB)
+테스트 24 〉	통과 (2.24ms, 72.4MB)
+테스트 25 〉	통과 (3.17ms, 72.8MB)
+테스트 26 〉	통과 (3.48ms, 77.8MB)
+테스트 27 〉	통과 (2.16ms, 74.4MB)
+테스트 28 〉	통과 (0.05ms, 77.1MB)
